ace3efffeaad0e6642dc71bd25bd6712afb2232a
tblandin <tiarra.blandin@outlook.com>
2022-09-25 12:19:50 -0600

    contact

--- ngToonThrowback/src/app/components/footer/footer.component.ts
+++ ngToonThrowback/src/app/components/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -10,6 +10,9 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class FooterComponent implements OnInit {
 
+  closeResult: string = '';
+  searchTerm: string = "";
+
   constructor(
     private modalService: NgbModal,
     private auth: AuthService,
@@ -17,6 +20,33 @@ export class FooterComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result: any) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason: any) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  contact() {
+        this.router.navigateByUrl('home');
   }
 
 }
