13b08efb63c62eae31d59b85ac5c766298c8d558
tblandin <tiarra.blandin@outlook.com>
2022-09-26 14:29:38 -0600

    toon display

--- ngToonThrowback/src/app/services/toon.service.ts
+++ ngToonThrowback/src/app/services/toon.service.ts
@@ -8,76 +8,67 @@ import { catchError, throwError } from 'rxjs';
 import { Media } from '../models/media';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToonService {
   private url = environment.baseUrl + 'api/cartoons';
 
-  constructor( private http: HttpClient,
-    private auth: AuthService) { }
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
-    index() {
-      return this.http.get<Toon[]>(this.url).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError(
-            () => new Error (
-              'ToonService.index(): error retrieving Toon List: ' + err
-            )
-          )
-        }
-        )
-        )
-    }
+  index() {
+    return this.http.get<Toon[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('ToonService.index(): error retrieving Toon List: ' + err)
+        );
+      })
+    );
+  }
 
-    search(searchTerm: string){
-      return this.http.get<Toon[]>(this.url + "/search/" + searchTerm).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError(
-            () => new Error('ToonService.index(): error retrieving cartoon: ' + err)
-          );
-        })
-      );
-    }
+  search(searchTerm: string) {
+    return this.http.get<Toon[]>(this.url + '/search/' + searchTerm).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('ToonService.index(): error retrieving cartoon: ' + err)
+        );
+      })
+    );
+  }
 
-    create(toon: Toon) {
-      return this.http.post<Toon>(this.url, toon).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError(
-            () => new Error (
-              'ToonService.create(): error creating Toon: ' + err
-            )
-          );
-        })
-      );
-    }
+  create(toon: Toon) {
+    return this.http.post<Toon>(this.url, toon).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('ToonService.create(): error creating Toon: ' + err)
+        );
+      })
+    );
+  }
 
-    update(toon: Toon) {
-      return this.http.put<Toon>(this.url + '/' + toon.id, toon).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError(
-            () => new Error (
-              'ToonService.update(): error updating Toon: ' + err
-            )
-          );
-        })
-      );
-    }
+  update(toon: Toon) {
+    return this.http.put<Toon>(this.url + '/' + toon.id, toon).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('ToonService.update(): error updating Toon: ' + err)
+        );
+      })
+    );
+  }
 
-    destroy(id: number) {
-      return this.http.delete<Toon>(this.url + '/' + id).pipe(
-        catchError((err: any) => {
-          console.log(err);
-          return throwError(
-            () => new Error (
-              'ToonService.destroy(): error deleting Toon: ' + err
-            )
-          );
-        })
-      );
-    }
-
+  destroy(id: number) {
+    return this.http.delete<Toon>(this.url + '/' + id).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('ToonService.destroy(): error deleting Toon: ' + err)
+        );
+      })
+    );
+  }
 }
