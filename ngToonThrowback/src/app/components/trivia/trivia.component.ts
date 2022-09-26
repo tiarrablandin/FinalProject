cd46a846669e65ee3464e450e24b338ad253f76b
Samantha Heisterkamp <samanthapaige912@yahoo.com>
2022-09-25 22:55:24 -0600

    trivia.component.ts

--- ngToonThrowback/src/app/components/trivia/trivia.component.ts
+++ ngToonThrowback/src/app/components/trivia/trivia.component.ts
@@ -1,3 +1,5 @@
+import { Trivia } from './../../models/trivia';
+import { TriviaService } from './../../services/trivia.service';
 import { Component, OnInit } from '@angular/core';
 import { popper } from '@popperjs/core';
 
@@ -8,9 +10,103 @@ import { popper } from '@popperjs/core';
 })
 export class TriviaComponent implements OnInit {
 
-  constructor() { }
+
+
+  selected: Trivia | null = null;
+
+  newTrivia: Trivia = new Trivia();
+
+  editTrivia: Trivia | null = null;
+
+  trivias: Trivia[] = [];
+
+  constructor(private triviaService: TriviaService) { }
+
 
   ngOnInit(): void {
+    this.reload();
   }
 
+
+  reload() {
+    this.triviaService.index().subscribe(
+    {
+      next: (data) => {
+        this.trivias = data
+      },
+      error: (err) => {
+        console.error('TriviaListComponent.reload(): error loading Trivia:');
+        console.error(err);
+      }
+    }
+    );
+  }
+
+
+
+  getNumOfTrivia() {
+    return this.trivias.length;
+
+  }
+
+  displayTrivia(trivia: Trivia) {
+  this.selected = trivia;
+  }
+
+  displayTable() {
+    this.selected = null;
+  }
+addTrivia() {
+this.triviaService.create(this.newTrivia).subscribe(
+  {
+    next: (data) => {
+      this.newTrivia = new Trivia();
+      this.reload();
+    },
+    error: (err) => {
+      console.error('TriviaComponent.create(): error creating Trivia:');
+      console.error(err);
+    }
+  }
+);
+
+
 }
+
+
+
+setEditTrivia() {
+  this.editTrivia = Object.assign({}, this.selected);
+}
+
+updateTrivia(updatedTrivia: Trivia) {
+this.triviaService.update(updatedTrivia).subscribe(
+  {
+    next: (data) => {
+      this.selected = data;
+      this.editTrivia = null;
+      this.reload();
+    },
+    error: (err) => {
+      console.error('TriviaListComponent.updateTrivia(): error updating Trivia:');
+      console.error(err);
+    }
+  }
+);
+}
+
+deleteTrivia(id: number) {
+this.triviaService.destroy(id).subscribe(
+  {
+    next: () => {
+      this.reload();
+    },
+    error: (err) => {
+      console.error('TriviaListComponent.deleteTrivia(): error deleting Trivia:');
+      console.error(err);
+    }
+  }
+);
+}
+}
+
