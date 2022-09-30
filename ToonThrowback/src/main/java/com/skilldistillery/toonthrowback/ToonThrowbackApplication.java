5d9fcd406c49463ed55feadc9d19ec4456b58d74
tblandin <tiarra.blandin@outlook.com>
2022-09-30 10:42:24 -0600

    small final tweaks

--- ToonThrowback/src/main/java/com/skilldistillery/toonthrowback/ToonThrowbackApplication.java
+++ ToonThrowback/src/main/java/com/skilldistillery/toonthrowback/ToonThrowbackApplication.java
@@ -2,13 +2,20 @@ package com.skilldistillery.toonthrowback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ToonThrowbackApplication {
-
+public class ToonThrowbackApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ToonThrowbackApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ToonThrowbackApplication.class, args);
 	}
